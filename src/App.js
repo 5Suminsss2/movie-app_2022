1891a360d96b76f431744a4f9c93b9f73d23aa5f
5Suminsss2 <1012ksm@naver.com>
2022-03-01 21:38:18 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import {useEffect, useState} from "react";
 
+function Hello() {
+  useEffect(()=> { //false일때 완전히 지워지고 true일때 다시 refresh되어서 show이면 콘솔창에 등장
+    console.log("created :) ");
+    return () =>  console.log("destroyed :( ");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev +1);
-  const onChange = (event) => setKeyword(event.target.value);
- 
-  //어떤 state가 바뀌든지 바뀌면 맨날 실행
-  console.log("I run all the time")
-  // 처음 시작할때만 실행
-  useEffect(() => {
-    console.log("I run only once");
-  }, []); 
-  //keyword가 변화할 때만 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]); 
-  //counter가 변화할 때만 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]); 
-  // 합치기 가능
-  useEffect(() => {
-    console.log("I run when 'counter' and 'keyword' changes");
-  }, [keyword, counter]); 
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   
   return (
     <div>
-      <input value={keyword} onChange={onChange} placeholder="Search..." type="text" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}> {showing ? "Hide" : "Show" }</button>
     </div>
   );
 }
