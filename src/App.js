51a0e69462636b357462d0b345cc66d1b463ae14
5Suminsss2 <1012ksm@naver.com>
2022-03-01 20:37:26 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev +1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
+ 
+  //어떤 state가 바뀌든지 바뀌면 맨날 실행
+  console.log("I run all the time")
+  // 처음 시작할때만 실행
   useEffect(() => {
-    console.log("CALL THE API..");
-  }, []);
+    console.log("I run only once");
+  }, []); 
+  //keyword가 변화할 때만 실행
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]); 
+  //counter가 변화할 때만 실행
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]); 
+  // 합치기 가능
+  useEffect(() => {
+    console.log("I run when 'counter' and 'keyword' changes");
+  }, [keyword, counter]); 
   
   return (
     <div>
+      <input value={keyword} onChange={onChange} placeholder="Search..." type="text" />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
