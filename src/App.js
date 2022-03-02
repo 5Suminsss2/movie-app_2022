66bc6c76a0f73f6e8f9d5f95c840416cce01b339
5Suminsss2 <1012ksm@naver.com>
2022-03-02 17:41:50 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  //코인 api는 이 서비스가 시작될때 한번만 실행되길 원해
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json)
-      //코인 가져오면 로딩 멈추기
-      setLoading(false);
-    });
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await(
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(()=> {
+    getMovies();
   },[]);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ): (
-      <select>
-        {coins.map((coin) => (
-          <option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>
-        ))}
-      </select>)}
-      
+    <div>{loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => 
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image} />
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map((g)=>(
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>)}</div>}
     </div>
   );
 }
