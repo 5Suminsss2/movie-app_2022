9340cdd7012ba942bb88c63c67c8c8f912e8e381
5Suminsss2 <1012ksm@naver.com>
2022-03-02 21:54:03 +0900

    #7.5 React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,28 @@
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
-function Movie({coverImg, title, summary, genres}) {
-    return (
-        <div>
-            <img src={coverImg} alt={title}/>
-            <h2>{title}</h2>
-            <p>{summary}</p>
-            <ul>
-            {genres.map((g)=>(
-                <li key={g}>{g}</li>
-            ))}
-            </ul>
-        </div>
-    );
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+          <Link to="/movie">{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    coverImg: propTypes.string.isRequired,
-    title:propTypes.string.isRequired,
-    summary:propTypes.string.isRequired,
-    genres: propTypes.arrayOf(propTypes.string).isRequired
-}
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
